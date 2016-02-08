7e73feacf055f364ee3c4eca812855b9d09437c9
Erik Pavletic <erik@kodamera.se>
2016-02-08 16:25:45 +0100

    fixed minor typo

--- _bootstrap/setup-bash.sh
+++ _bootstrap/setup-bash.sh
@@ -33,53 +33,53 @@ nodot=("Brewfile") # list of non-dot files to symlink
 
 # Install the Fira Code font.
 #==========================================================
-msg 'Fetching Fira Code from Github…'
-git clone https://github.com/tonsky/FiraCode.git ${tmpdir}
+# msg 'Fetching Fira Code from Github…'
+# git clone https://github.com/tonsky/FiraCode.git ${tmpdir}
 
-msg "Moving Fira Code font-files to ${code}${fontdir}${end}${grey}…"
-find ${tmpdir} -name 'FiraCode-*.otf' -exec mv -i {} ${fontdir} \;
-don
+# msg "Moving Fira Code font-files to ${code}${fontdir}${end}${grey}…"
+# find ${tmpdir} -name 'FiraCode-*.otf' -exec mv -i {} ${fontdir} \;
+# don
 
-msg "Cleaning up…"
-rm -rf ${tmpdir}
-don
+# msg "Cleaning up…"
+# rm -rf ${tmpdir}
+# don
 
 # Install the Wombal.terminal theme and set as default.
 #==========================================================
-msg "Setting up Terminal.app"
-osascript <<EOD
-tell application "Terminal"
-  local allOpenedWindows
-  local initialOpenedWindows
-  local windowID
-  set themeName to "Wombat"
-  (* Store the IDs of all the open terminal windows. *)
-  set initialOpenedWindows to id of every window
-  (* Open the custom theme so that it gets added to the list
-     of available terminal themes (note: this will open two
-     additional terminal windows). *)
-  do shell script "open '$HOME/.dotfiles/" & themeName & ".terminal'"
-  (* Wait a little bit to ensure that the custom theme is added. *)
-  delay 1
-  (* Set the custom theme as the default terminal theme. *)
-  set default settings to settings set themeName
-  (* Get the IDs of all the currently opened terminal windows. *)
-  set allOpenedWindows to id of every window
-  repeat with windowID in allOpenedWindows
-    (* Close the additional windows that were opened in order
-       to add the custom theme to the list of terminal themes. *)
-    if initialOpenedWindows does not contain windowID then
-      close (every window whose id is windowID)
-    (* Change the theme for the initial opened terminal windows
-       to remove the need to close them in order for the custom
-       theme to be applied. *)
-    else
-      set current settings of tabs of (every window whose id is windowID) to settings set themeName
-    end if
-  end repeat
-end tell
-EOD
-don
+# msg "Setting up Terminal.app"
+# osascript <<EOD
+# tell application "Terminal"
+#   local allOpenedWindows
+#   local initialOpenedWindows
+#   local windowID
+#   set themeName to "Wombat"
+#   (* Store the IDs of all the open terminal windows. *)
+#   set initialOpenedWindows to id of every window
+#   (* Open the custom theme so that it gets added to the list
+#      of available terminal themes (note: this will open two
+#      additional terminal windows). *)
+#   do shell script "open '$HOME/.dotfiles/" & themeName & ".terminal'"
+#   (* Wait a little bit to ensure that the custom theme is added. *)
+#   delay 1
+#   (* Set the custom theme as the default terminal theme. *)
+#   set default settings to settings set themeName
+#   (* Get the IDs of all the currently opened terminal windows. *)
+#   set allOpenedWindows to id of every window
+#   repeat with windowID in allOpenedWindows
+#     (* Close the additional windows that were opened in order
+#        to add the custom theme to the list of terminal themes. *)
+#     if initialOpenedWindows does not contain windowID then
+#       close (every window whose id is windowID)
+#     (* Change the theme for the initial opened terminal windows
+#        to remove the need to close them in order for the custom
+#        theme to be applied. *)
+#     else
+#       set current settings of tabs of (every window whose id is windowID) to settings set themeName
+#     end if
+#   end repeat
+# end tell
+# EOD
+# don
 
 # create dotfiles_old in homedir
 msg "Creating $olddir for backup of any existing dotfiles in $HOME..."
