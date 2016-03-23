1636ea95bd9626b592b7dc6a48e73ddd8f29dd23
Erik Pavletic <erik.pavletic@gmail.com>
2016-03-23 14:02:19 +0100

    Made sure that the git prompt is seen.

--- bash/ps1.sh
+++ bash/ps1.sh
@@ -1,13 +1,13 @@
 #------ PROMPT STYLE ------------------------------------
 boldGreen=$(tput setaf 052;tput bold)
-grey=$(tput setaf 235)
+grey=$(tput setaf 241)
 lightBlue=$(tput setaf 75)
 end=$(tput sgr0)
 
 # \n – New line
 # \d – Current date
-# \t - The time, in 24-hour HH:MM:SS format. 
-# \T - The time, in 12-hour HH:MM:SS format. 
+# \t - The time, in 24-hour HH:MM:SS format.
+# \T - The time, in 12-hour HH:MM:SS format.
 # \@ - The time, in 12-hour am/pm format.
 # \h – Host name
 # \# – Command number
@@ -17,6 +17,4 @@ end=$(tput sgr0)
 # \[ - Begin a sequence of non-printing characters, which could be used to embed a terminal control sequence into the prompt
 # \] - End a sequence of non-printing characters
 
-
 export PS1="\n${boldGreen}\w${end}${grey}\$(__git_ps1)\n\[${lightBlue}\]❯ \[${end}\]"
-# export PS1="\n$(tput setaf 051;tput bold)\w$(tput sgr0)$(tput setaf 248)\$(__git_ps1)\n\[$(tput setaf 111)\]❯ \[$(tput sgr0)\]"
