204753bdd06e35761d2e3f8cb2612a4385dcc8d3
Erik Pavletic <erik.pavletic@gmail.com>
2016-03-25 18:44:21 +0100

    Tweaks for better output

--- _bootstrap/setup-homebrew.sh
+++ _bootstrap/setup-homebrew.sh
@@ -3,7 +3,7 @@
 #==========================================================
 #
 # This script will setup Homebrew for OSX, and install a
-# number of formulaes and casks (/apps), all specified in 
+# number of formulaes and casks (/apps), all specified in
 # the Brewfile in the .dotfiles root.
 #
 #==========================================================
@@ -15,16 +15,39 @@ code=$(tput setab 236; tput setaf 253)
 end=$(tput sgr0)
 
 msg() {
-  printf "${grey}$*${end}\n"
+  printf "\n${grey}$*${end}\n"
 }
 
-don() {
-  printf "$green\xE2\x9c\x94 Done! ${end}\n\n"
+fin() {
+  printf "\n$green\xE2\x9c\x94 $*${end}\n\n"
 }
+
+# Make shure we're in the home-directory.
+cd $HOME
 
 if [[ `uname` == "Darwin" ]]; then
-  msg "Installing Homebrew and the formulae/casks specified in the Brewfile..."
-  type brew &>/dev/null && printf "`brew bundle`" || echo "`ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\" && brew tap Homebrew/bundle && brew bundle`"
-  brew update && brew upgrade `brew outdated`
+  msg "Installing Homebrew and the formulae/casks specified in the Brewfile…"
+
+  which -s brew
+  if [[ $? != 0 ]] ; then
+    # Install Homebrew.
+    # https://github.com/Homebrew/homebrew/tree/master/share/doc/homebrew#readme
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  else
+    msg "Homebrew appears to be already installed.\nUpdating packages list…"
+    brew update
+  fi
+
+  # Fetch the Brew bundle command, and excecute it.
+  brew tap homebrew/bundle
+  brew bundle
+
+  # Add CoreUtils to $PATH
+  echo '
+# CoreUtils - switch from BSD to Linux/GNU command line tools.
+PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"' >> ~/.dotfiles/bash/path.sh
+
   don
 fi
+
+fin "Homebrew is up and running.\nYou should open a new Terminal-window in order for the CoreUtils settings to take effect."
