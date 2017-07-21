646f38c648dfcd4ddfd40cbddd64ece72dd1d44a
Erik Pavletic <erik.pavletic@gmail.com>
2017-07-21 16:06:57 +0200

    Added Yarn executable to path.

--- bash/exports.sh
+++ bash/exports.sh
@@ -3,6 +3,9 @@ export CLICOLOR=1
 export LS_OPTIONS='--color=auto'
 # export LSCOLORS=ExFxCxDxBxegedabagacad
 
+# Yarn Package Manager
+export PATH="$HOME/.yarn/bin:$PATH"
+
 # Node Version Manager
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
