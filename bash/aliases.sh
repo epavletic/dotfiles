4cc70b63de451bd5d18bbd6d7498722c512df13b
Erik Pavletic <erik.pavletic@gmail.com>
2017-08-16 18:48:48 +0200

    Switch to use VS Code for the 'dotfiles' alias.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -3,7 +3,7 @@ alias sub='open -a "/Applications/Sublime Text.app"'
 
 
 # Bash
-alias dotfiles='sub ~/.dotfiles'
+alias dotfiles='code ~/.dotfiles'
 alias reload='source ~/.bash_profile; echo ".bash_profile reloaded"'
 alias colors='( x=`tput op` y=`printf %$((${COLUMNS}-6))s`;for i in {0..256};do o=00$i;echo -e ${o:${#o}-3:3} `tput setaf $i;tput setab $i`${y// /=}$x;done; )'
 alias ls='ls -lAhF --group-directories-first --color'
