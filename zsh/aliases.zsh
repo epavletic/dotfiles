2162242e240862ff389a61e882b1d271462f0aa7
Erik Pavletic <erik.pavletic@etraveligroup.com>
2022-07-25 17:07:21 +0200

    Tweak some of the terminal aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,12 +1,13 @@
 #░░░░░░░░ ALIASES ░░░░░░░░#
 
 # Misc.
-alias dotfiles='code ~/.dotfiles'
-alias reload='source ~/.zshrc; echo ".zshrc reloaded"'
+alias codeo='open . -a "Visual Studio Code"'
+alias dotfiles='open ~/.dotfiles -a "Visual Studio Code"'
+alias reload='source ~/.zshrc; echo "\n╔════════════════════╗\n║ ✅ .zshrc reloaded ║\n╚════════════════════╝"'
 alias lsa='ls -lAhF --group-directories-first --color'
 
 # Git
-alias gh="git log --format='%Cgreen%h%Creset %C(cyan)%an%Creset - %s' --graph"
+alias gh="git log --format='%Cgreen%h%Creset %C(cyan)%an%Creset %C(yellow)(%cr)%Creset %n%s%n' --graph"
 alias gs='git status'
 alias gl='git pull'
 alias gp='git push'
